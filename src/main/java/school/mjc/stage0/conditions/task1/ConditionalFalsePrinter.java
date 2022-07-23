6e58e4bd0bcea5821f257032b876f71163c24ed0
atainaive <atai.omorov@stud.vgtu.lt>
2022-07-23 16:10:47 +0300

    commit

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,7 +2,8 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
-        if(false){
+        if (false) {
+        } else {
             System.out.println("false");
         }
     }
